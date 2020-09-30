c9fe41233f417e5e9a7e5e8dc756ff049c8d2346
Luke Strickland <lukejgstrickland@gmail.com>
2020-09-30 09:23:00 +0800

    results changes for final drafting

--- R/grid_dispatch.R
+++ R/grid_dispatch.R
@@ -1,46 +1,8 @@
 
 source("dmc/dmc.R")
 
-
-load_model("LBA", "lba_B.R")
-run.grid.dmc("CA_top_samples",model.dir ="LBA",
-             model.file="lba_B.R",user="ljs392",
-             n.add=60, wall.hours = 300,
-             GB = 2, max.try=5)
-
 load_model("LBA", "lba_B_autothres.R")
 run.grid.dmc("CA_top_thresholdsmult_samples",model.dir ="LBA",
              model.file="lba_B_autothres.R",user="ljs392",
              n.add=60, wall.hours = 300,
-             GB = 2, max.try=5)
-
-load_model("LBA","lba_B_automation.R")
-run.grid.dmc("auto_top_samples",model.dir ="LBA",
-             model.file="lba_B_automation.R",user="ljs392",
-             n.add=60, wall.hours = 300,
-             GB = 2, max.try=5)
-
-load_model("LBA","lba_B_automation.R")
-run.grid.dmc("auto_noB_samples",model.dir ="LBA",
-             model.file="lba_B_automation.R",user="ljs392",
-             n.add=60, wall.hours = 300,
-             GB = 2, max.try=5)
-
-load_model("LBA","lba_B_automation.R")
-run.grid.dmc("auto_anoS_samples",model.dir ="LBA",
-             model.file="lba_B_automation.R",user="ljs392",
-             n.add=60, wall.hours = 300,
-             GB = 2, max.try=5)
-
-load_model("LBA","lba_B_automation.R")
-run.grid.dmc("auto_anoSnoB_samples",model.dir ="LBA",
-             model.file="lba_B_automation.R",user="ljs392",
-             n.add=60, wall.hours = 300,
-             GB = 2, max.try=5)
-
-load_model("LBA","lba_B_automation.R")
-run.grid.dmc("auto_anoS_ex_samples",model.dir ="LBA",
-             model.file="lba_B_automation.R",user="ljs392",
-             n.add=60, wall.hours = 300,
-             GB = 2, max.try=5)
-
+             GB = 2, max.try=30)
